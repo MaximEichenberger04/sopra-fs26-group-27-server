42d13e82ecfcc61dc10d04185c178476b1306ad9
Maxim <maximjean.eichenberger@uzh.ch>
2026-03-25 17:39:53 +0100

    createLobby logic, lobby entity and postDTO

--- src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/LobbyPostDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/LobbyPostDTO.java
@@ -2,4 +2,40 @@ package ch.uzh.ifi.hase.soprafs26.rest.dto;
 
 public class LobbyPostDTO {
     
+    private String name;
+    private Long hostId;
+    private String gameMode;
+    private Integer playerCount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getHostId() {
+        return hostId;
+    }
+
+    public void setHostId(Long hostId) {
+        this.hostId = hostId;
+    }
+
+    public String getGameMode() {
+        return gameMode;
+    }
+
+    public void setGameMode(String gameMode) {
+        this.gameMode = gameMode;
+    }
+
+    public Integer getPlayerCount() {
+        return playerCount;
+    }
+
+    public void setPlayerCount(Integer playerCount) {
+        this.playerCount = playerCount;
+    }
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/LobbyService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/LobbyService.java
@@ -7,14 +7,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Transactional
 public class LobbyService {
 
-    public List<Lobby> getLobbies() {
-        return new ArrayList<>();
-    }
+    private final Logger log = LoggerFactory.getLogger(LobbyService.class);
 
-    public Lobby createLobby(Lobby lobby, String token) {
-        return lobby;
+	private final LobbyRepository lobbyRepository;
+
+	public LobbyService(@Qualifier("lobbyRepository") LobbyRepository lobbyRepository) {
+		this.lobbyRepository = lobbyRepository;
+	}
+
+	public Lobby createLobby(Lobby newLobby) {
+        if (newLobby.getName() == null || newLobby.getName().isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Lobby name must not be empty");
+        }
+
+        if (newLobby.getHostId() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "HostId must not be null");
+        }
+
+        if (newLobby.getPlayerCount() == null || newLobby.getPlayerCount() != 2 || newLobby.getPlayerCount() != 4) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Player count must be 2 or 4");
+        }
+
+		newLobby.setStatus(LobbyStatus.WAITING);
+		newLobby.setCurrentPlayers(1);
+        newLobby.setInviteCode(generateInviteCode());
+
+		newLobby = lobbyRepository.save(newLobby);
+		lobbRepository.flush();
+
+		log.debug("Created Lobby: {}", newLobby);
+		return newLobby;
+	}
+
+    public List<Lobby> getLobbies() {
+        return this.lobbyRepository.findAll();
     }
 
     public Lobby getLobbyById(Long lobbyId) {
@@ -36,4 +65,10 @@ public class LobbyService {
     public void leaveLobby(Long lobbyId, String token) {
         // TODO
     }
+    
+    // HELPER METHOD FOR CREATELOBBY
+    public string generateInviteCode() {
+        return UUID.randomUUID().toString().replace("-", "").substring(0, 6).toUpperCase(); // generate UUID and take the first 6 chars
+    }
 }
+    
