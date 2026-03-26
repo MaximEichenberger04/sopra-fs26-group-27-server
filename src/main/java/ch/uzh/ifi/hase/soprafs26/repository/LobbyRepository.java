c4953afef0a66e8d8ebe7deb41d5e2c263ddc965
Maxim Eichenberger <maxim.eichenberger@outlook.com>
2026-03-26 18:19:39 +0100

    Lobby update function, LobbyPutDTO, Repository, LobbyStatus enum

--- src/main/java/ch/uzh/ifi/hase/soprafs26/repository/LobbyRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/repository/LobbyRepository.java
@@ -1,5 +1,11 @@
 package ch.uzh.ifi.hase.soprafs26.repository;
 
-public class LobbyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.uzh.ifi.hase.soprafs26.entity.Lobby;
+
+@Repository("lobbyRepository")
+public interface lobbyRepository extends JpaRepository<Lobby, Long> {
     
 }
--- src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/LobbyPutDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/LobbyPutDTO.java
@@ -2,4 +2,31 @@ package ch.uzh.ifi.hase.soprafs26.rest.dto;
 
 public class LobbyPutDTO {
     
+    private String name;
+    private String gameMode;
+    private Integer playerCount;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGameMode() {
+        return gameMode;
+    }
+
+    public void setGameMode(String gameMode) {
+        this.gameMode = gameMode;
+    }
+
+    public Integer getPlayerCount() {
+        return playerCount;
+    }
+
+    public void setPlayerCount(Integer playerCount) {
+        this.playerCount = playerCount;
+    }
 }
